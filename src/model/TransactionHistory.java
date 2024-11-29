cae17079d8e61fce92394cddc104cfbc7246733e
Thevid Oswald Berrius <thevidob@gmail.com>
2024-11-29 16:12:04 +0700

    Added History Model

---
+++ src/model/TransactionHistory.java
@@ -0,0 +1,56 @@
+package model;
+
+public class TransactionHistory {
+	private String transactionId, itemName, itemCategory, itemSize, itemPrice;
+
+	public TransactionHistory(String transactionId, String itemName, String itemCategory, String itemSize,
+			String itemPrice) {
+		super();
+		this.transactionId = transactionId;
+		this.itemName = itemName;
+		this.itemCategory = itemCategory;
+		this.itemSize = itemSize;
+		this.itemPrice = itemPrice;
+	}
+
+	public String getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(String transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemCategory() {
+		return itemCategory;
+	}
+
+	public void setItemCategory(String itemCategory) {
+		this.itemCategory = itemCategory;
+	}
+
+	public String getItemSize() {
+		return itemSize;
+	}
+
+	public void setItemSize(String itemSize) {
+		this.itemSize = itemSize;
+	}
+
+	public String getItemPrice() {
+		return itemPrice;
+	}
+
+	public void setItemPrice(String itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	
+}
