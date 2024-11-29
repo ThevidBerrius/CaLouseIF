f221c0ef58da66546def88d38cbd57d829cdc52d
Thevid Oswald Berrius <thevidob@gmail.com>
2024-11-29 12:58:15 +0700

    Added Transaction Model

---
+++ src/model/Transaction.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Transaction {
+	private String transactionId, userId, itemId;
+
+	public Transaction(String transactionId, String userId, String itemId) {
+		super();
+		this.transactionId = transactionId;
+		this.userId = userId;
+		this.itemId = itemId;
+	}
+
+	public String getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(String transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+	
+}
