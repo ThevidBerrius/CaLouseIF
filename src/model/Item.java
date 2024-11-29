31e96ae463401879fa3a1ad349cade56dfcdedb9
Thevid Oswald Berrius <thevidob@gmail.com>
2024-11-29 12:35:00 +0700

    Getset item

--- src/model/Item.java
+++ src/model/Item.java
@@ -2,6 +2,55 @@ package model;
 
 public class Item {
 	private String itemId, itemName, itemSize, itemPrice, itemCategory;
+
+	public Item(String itemId, String itemName, String itemSize, String itemPrice, String itemCategory) {
+		super();
+		this.itemId = itemId;
+		this.itemName = itemName;
+		this.itemSize = itemSize;
+		this.itemPrice = itemPrice;
+		this.itemCategory = itemCategory;
+	}
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemSize() {
+		return itemSize;
+	}
+
+	public void setItemSize(String itemSize) {
+		this.itemSize = itemSize;
+	}
+
+	public String getItemPrice() {
+		return itemPrice;
+	}
+
+	public void setItemPrice(String itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+	public String getItemCategory() {
+		return itemCategory;
+	}
+
+	public void setItemCategory(String itemCategory) {
+		this.itemCategory = itemCategory;
+	}
 	
 	
 }
