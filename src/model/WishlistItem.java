75ccb09b37f5050469fb50fd99cc0c26eac8818d
Thevid Oswald Berrius <thevidob@gmail.com>
2024-11-29 16:19:20 +0700

    Added model wishlist item
    model untuk data wishlist

---
+++ src/model/WishlistItem.java
@@ -0,0 +1,92 @@
+package model;
+
+public class WishlistItem {
+	private String wishlistId, userId, itemId, itemName, itemSize, itemCategory, itemStatus, itemWishlist, itemOfferStatus;
+
+	public WishlistItem(String wishlistId, String userId, String itemId, String itemName, String itemSize,
+			String itemCategory, String itemStatus, String itemWishlist, String itemOfferStatus) {
+		super();
+		this.wishlistId = wishlistId;
+		this.userId = userId;
+		this.itemId = itemId;
+		this.itemName = itemName;
+		this.itemSize = itemSize;
+		this.itemCategory = itemCategory;
+		this.itemStatus = itemStatus;
+		this.itemWishlist = itemWishlist;
+		this.itemOfferStatus = itemOfferStatus;
+	}
+
+	public String getWishlistId() {
+		return wishlistId;
+	}
+
+	public void setWishlistId(String wishlistId) {
+		this.wishlistId = wishlistId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemSize() {
+		return itemSize;
+	}
+
+	public void setItemSize(String itemSize) {
+		this.itemSize = itemSize;
+	}
+
+	public String getItemCategory() {
+		return itemCategory;
+	}
+
+	public void setItemCategory(String itemCategory) {
+		this.itemCategory = itemCategory;
+	}
+
+	public String getItemStatus() {
+		return itemStatus;
+	}
+
+	public void setItemStatus(String itemStatus) {
+		this.itemStatus = itemStatus;
+	}
+
+	public String getItemWishlist() {
+		return itemWishlist;
+	}
+
+	public void setItemWishlist(String itemWishlist) {
+		this.itemWishlist = itemWishlist;
+	}
+
+	public String getItemOfferStatus() {
+		return itemOfferStatus;
+	}
+
+	public void setItemOfferStatus(String itemOfferStatus) {
+		this.itemOfferStatus = itemOfferStatus;
+	}
+	
+}
